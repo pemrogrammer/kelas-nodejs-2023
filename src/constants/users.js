fc11e6453eaa0e2bfaa2c199d89e473c78cda5c9
egin10 <egin066@gmail.com>
2023-03-12 21:20:55 +0700

    set data user

---
+++ src/constants/users.js
@@ -0,0 +1,39 @@
+const dataUsers = [
+  {
+    id: 1,
+    name: "Rahmat",
+    grade: "A",
+  },
+  {
+    id: 2,
+    name: "Bayu",
+    grade: "B",
+  },
+  {
+    id: 3,
+    name: "Dian",
+    grade: "C",
+  },
+  {
+    id: 4,
+    name: "Jhon",
+    grade: "D",
+  },
+  {
+    id: 5,
+    name: "Tulip",
+    grade: "E",
+  },
+  {
+    id: 6,
+    name: "Toyip",
+    grade: "E",
+  },
+  {
+    id: 7,
+    name: "Bambang",
+    grade: "E",
+  },
+];
+
+module.exports = dataUsers;
